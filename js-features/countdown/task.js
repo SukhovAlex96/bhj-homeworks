b9b6abaf127eb87324e69c6011dbf922ae31edbf
Alex_Sukhov <Alekc-suhov@yandex.ru>
2023-06-15 18:33:12 +0300

    homework

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,1 +1,9 @@
-1
+function countdown(){
+    seconds = (document.getElementById("timer").textContent) --;
+    if (seconds <= 0){
+        clearTimeout(timer);
+        alert("Вы победили в конкурсе!");
+    }else{
+        setTimeout(countdown, 1000);
+    }
+}
