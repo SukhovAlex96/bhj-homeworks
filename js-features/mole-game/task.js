82097e274d22e407d89f2e673cdd26d3145f866c
Alex_Sukhov <Alekc-suhov@yandex.ru>
2023-06-19 20:49:14 +0300

    homework

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,20 +1,23 @@
-for (var index = 1; index <= 9; index ++){
-    document.getElementById(`hole${index}`).onclick = HoleClick;
-}
-var aCatch = 0;
-var aMissed =0;
+const dead = document.getElementById('dead');
+const lost = document.getElementById('lost');
 
-function HoleClick(){
-   if(this.classList.contains('hole_has-mole')){
-        aCatch += 1;
-    }else{
-        aMissed += 1;
+const reload = () => {
+    dead.textContent = 0;
+    lost.textContent = 0;
+}
+const getHole = index =>{
+    return index = document.getElementById(`hole${index}`);
+}
+
+for (let i = 1; i <= 9; i++){
+    getHole(i).addEventListener('click', func = () => {getHole(i).className.includes('hole_has-mole') ? dead.textContent++ : lost.textContent++
+    if(dead.textContent == 10){
+        alert("Вы победили!");
+        reload();
     }
-   if(aCatch === 10 || aMissed === 5){
-    alert(aCatch === 10? "Вы победили !!!" : "Вы проиграли...");
-    aCatch = 0;
-    aMissed = 0;
-   } 
-   document.getElementById("dead").textContent = aCatchCatch;
-   document.getElementById("lost").textContent = aMissedMissed;
+    if(lost.textContent == 5){
+        alert("Поражение(");
+        reload();
+    }
+});
 }
